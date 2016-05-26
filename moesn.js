13ad908c36aa647de4716007c596b880b59b1f0b
moesn <475484467@qq.com>
2016-05-26 19:45:41 +0800

    zhaohuaming

--- moesn.js
+++ moesn.js
@@ -3,18 +3,22 @@
  */
 'use strict';
 
+const _ = require('lodash');
+
 function uuid() {
-    var d = new Date().getTime();
     var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
+        var r = (new Date().getTime() + Math.random() * 16) % 16 | 0;
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 };
 
-function replaceAll(str, pattern, replacement) {
-    return str.replace(new RegExp(pattern, 'g'), replacement);
+function replaceAll() {
+    var str = arguments[0];
+    _.chunk(_.drop(arguments), 2).forEach(function(arg) {
+        str = str.replace(new RegExp(arg[0], 'g'), arg[1]);
+    });
+    return str;
 };
 
 module.exports = {
